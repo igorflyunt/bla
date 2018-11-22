ee0ad7b5795ac01f95c0d42a95a68ab7f5499961
Igor Flyunt <igflyunt@gmail.com>
2018-11-22 22:31:25 +0200

    update Dao interface

--- src/main/java/com/softserve/librarymanager/dao/Dao.java
+++ src/main/java/com/softserve/librarymanager/dao/Dao.java
@@ -1,13 +1,17 @@
 package com.softserve.librarymanager.dao;
 
+import com.softserve.librarymanager.dao.table.Column;
+import com.softserve.librarymanager.dao.table.Table;
+import com.softserve.librarymanager.dao.table.TableDefinition;
+
 import java.util.List;
 
-public interface Dao<E> {
-    List<E> findAll();
+public interface Dao<E, T extends Enum<Table>, C extends Enum<C> & Column> {
+    List<E> findAll(TableDefinition<T, C> tableDefinition);
 
-    E findById(int id);
+    E findById(TableDefinition<T, C> tableDefinition, int id);
 
-    E saveOrUpdate(E entity);
+    E saveOrUpdate(TableDefinition<T, C> tableDefinition, E entity);
 
-    void delete(E entity);
+    void delete(TableDefinition<T, C> tableDefinition, E entity);
 }
