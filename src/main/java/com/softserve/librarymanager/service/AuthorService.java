1d7b79c7ffe79dd5bfc162a6094eaa859e59c97c
Igor Flyunt <igflyunt@gmail.com>
2018-11-25 14:04:24 +0200

    improve author service

--- src/main/java/com/softserve/librarymanager/service/AuthorService.java
+++ src/main/java/com/softserve/librarymanager/service/AuthorService.java
@@ -1,34 +1,27 @@
 package com.softserve.librarymanager.service;
 
 import com.softserve.librarymanager.dao.AuthorDao;
-import com.softserve.librarymanager.dao.BookDao;
 import com.softserve.librarymanager.dao.GenreDao;
 import com.softserve.librarymanager.dao.impl.AuthorDaoImpl;
-import com.softserve.librarymanager.dao.impl.BookDaoImpl;
 import com.softserve.librarymanager.dao.impl.GenreDaoImpl;
 import com.softserve.librarymanager.model.Author;
-import com.softserve.librarymanager.model.Book;
-import com.softserve.librarymanager.model.Genre;
 
-import java.util.List;
 import java.util.Optional;
 
 public class AuthorService {
+    private BookService bookService = new BookService();
     private AuthorDao authorDao = new AuthorDaoImpl();
-    private BookDao bookDao = new BookDaoImpl();
 
     private GenreDao genreDao = new GenreDaoImpl();
 
     public Optional<Author> findAuthorById(int authorId) {
-        return authorDao.findById(authorId).map(this::getAuthorFullData);
+        return authorDao.findById(authorId).map(this::initAuthorEagerly);
     }
 
-    private Author getAuthorFullData(Author author) {
+    private Author initAuthorEagerly(Author author) {
         int authorId = author.getId();
-        List<Genre> genres = genreDao.findGenresByAuthorId(authorId);
-        List<Book> books = bookDao.findAllBooksByAuthorId(authorId);
-        author.setGenres(genres);
-        author.setBooks(books);
+        author.setGenres(genreDao.findGenresByAuthorId(authorId));
+        author.setBooks(bookService.findAllBooksByAuthorId(authorId));
         return author;
     }
 }
