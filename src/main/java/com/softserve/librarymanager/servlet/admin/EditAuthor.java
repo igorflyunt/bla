fbe69130cd1325eaaea8162445794233a9f6de41
Igor Flyunt <igflyunt@gmail.com>
2018-11-25 18:29:15 +0200

    update admin's uris

--- src/main/java/com/softserve/librarymanager/servlet/admin/EditAuthor.java
+++ src/main/java/com/softserve/librarymanager/servlet/admin/EditAuthor.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "EditAuthor")
+@WebServlet("/admin/author")
 public class EditAuthor extends HttpServlet {
     protected void doPost(HttpServletRequest request,
                           HttpServletResponse response)
