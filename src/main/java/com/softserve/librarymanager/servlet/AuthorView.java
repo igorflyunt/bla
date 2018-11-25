231c6b90f4f3f592bf46339d72abe5b13bd872d5
Igor Flyunt <igflyunt@gmail.com>
2018-11-25 14:05:04 +0200

    implement author details jsp

--- src/main/java/com/softserve/librarymanager/servlet/AuthorView.java
+++ src/main/java/com/softserve/librarymanager/servlet/AuthorView.java
@@ -1,19 +1,28 @@
 package com.softserve.librarymanager.servlet;
 
+import com.softserve.librarymanager.model.Author;
+import com.softserve.librarymanager.service.AuthorService;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Optional;
 
 @WebServlet("/author")
 public class AuthorView extends HttpServlet {
+    private AuthorService authorService = new AuthorService();
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        System.out.println("test author endpoint");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        int authorId = Integer.parseInt(request.getParameter("authorId"));
+        authorService.findAuthorById(authorId).ifPresent(author -> request.setAttribute("author", author));
+        request.getRequestDispatcher("WEB-INF/AuthorView.jsp").forward(request, response);
     }
 }
