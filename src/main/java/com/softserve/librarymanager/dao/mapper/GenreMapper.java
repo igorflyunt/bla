91b90749ce5c4bce4590642d07413b2d5ba13c27
Igor Flyunt <igflyunt@gmail.com>
2018-11-26 12:16:10 +0200

    optimize imports

--- src/main/java/com/softserve/librarymanager/dao/mapper/GenreMapper.java
+++ src/main/java/com/softserve/librarymanager/dao/mapper/GenreMapper.java
@@ -1,14 +1,13 @@
 package com.softserve.librarymanager.dao.mapper;
 
-import com.softserve.librarymanager.dao.table.column.GenreColumns;
-import com.softserve.librarymanager.dao.table.util.ColumnUtil;
 import com.softserve.librarymanager.model.Genre;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import static com.softserve.librarymanager.dao.table.column.GenreColumns.*;
-import static com.softserve.librarymanager.dao.table.util.ColumnUtil.*;
+import static com.softserve.librarymanager.dao.table.column.GenreColumns.ID;
+import static com.softserve.librarymanager.dao.table.column.GenreColumns.NAME;
+import static com.softserve.librarymanager.dao.table.util.ColumnUtil.addAliasIfProvided;
 
 public class GenreMapper extends AbstractMapper<Genre> implements EntityMapper<Genre> {
     public GenreMapper() {
