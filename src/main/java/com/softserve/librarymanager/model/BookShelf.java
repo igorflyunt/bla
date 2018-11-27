40656528029ecd20df271329658062e55f290b2f
Igor Flyunt <igflyunt@gmail.com>
2018-11-27 23:45:51 +0200

    update bookshelf entity

--- src/main/java/com/softserve/librarymanager/model/BookShelf.java
+++ src/main/java/com/softserve/librarymanager/model/BookShelf.java
@@ -1,22 +1,28 @@
 package com.softserve.librarymanager.model;
 
 public class BookShelf extends AbstractEntity {
-    public enum BookShelfStatus {
-        READ,
-        WANT_TO_READ,
-        CURRENTLY_READING,
-        TO_READ
+    public enum BookShelves {
+        READ ("Read"),
+        WANT_TO_READ ("Want to read"),
+        CURRENTLY_READING ("Currently reading"),
+        TO_READ ("To-read");
+
+        private String bookShelfName;
+
+        BookShelves(String bookShelfName) {
+            this.bookShelfName = bookShelfName;
+        }
     }
 
-    private BookShelfStatus status;
+    private BookShelves status;
 
     private BookShelf() { }
 
-    public BookShelfStatus getStatus() {
-        return status;
+    public String getBookShelfName() {
+        return status.bookShelfName;
     }
 
-    public void setStatus(BookShelfStatus status) {
+    public void setStatus(BookShelves status) {
         this.status = status;
     }
 }
