ca6d5ff23b8dbe4570f286d47a366b0ef19c96bc
Igor Flyunt <igflyunt@gmail.com>
2018-11-28 02:33:07 +0200

    update user entity

--- src/main/java/com/softserve/librarymanager/model/User.java
+++ src/main/java/com/softserve/librarymanager/model/User.java
@@ -3,13 +3,16 @@ package com.softserve.librarymanager.model;
 import java.util.List;
 
 public class User extends Person {
+    private static final UserRole DEFAULT_ROLE = UserRole.USER;
+
+    private String username;
+    private String password;
 
     private List<Book> books;
-    private static final UserRole DEFAULT_ROLE = UserRole.USER;
 
     private UserRole role = DEFAULT_ROLE;
 
-    private User() { }
+    public User() { }
 
     public List<Book> getBooks() {
         return books;
@@ -25,6 +28,26 @@ public class User extends Person {
 
     public void setRole(UserRole role) {
         this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
     }
 
     @Override
